1d7b1a85323f9b3939f93de3c709f83877ebf428
kozakana <goki727@gmail.com>
2019-03-10 01:03:05 +0900

    Supports multiple files

--- grayscale.rb
+++ grayscale.rb
@@ -1,1 +1,8 @@
-system "pdftk /pdf/input_pdf/a.pdf cat 3-r2 output /pdf/output_pdf/a.pdf"
+INPUT_PATH  = '/pdf/input_pdf'
+OUTPUT_PATH = '/pdf/output_pdf'
+
+pdf_files = Dir.glob(File.join(INPUT_PATH, '*.pdf'))
+pdf_files.each do |input_pdf|
+  output_pdf = File.join(OUTPUT_PATH, File.basename(input_pdf))
+  system "pdftk #{input_pdf} cat 3-r2 output #{output_pdf}"
+end
